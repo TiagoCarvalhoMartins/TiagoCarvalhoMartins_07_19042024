2d1a4b1b2e9b4c98b34b30d17c83fd79ead299eb
TiagoCarvalhoMartins <86134696+TiagoCarvalhoMartins@users.noreply.github.com>
2024-05-31 17:27:51 +0200

    test

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,5 +1,6 @@
 const mongoose = require ('mongoose');
 
+<<<<<<< HEAD
 const ratingSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     grade: { type: Number, required: true }
@@ -14,26 +15,32 @@ const bookSchema = mongoose.Schema ({
     genre: { type: String, required: true },
     ratings: [ratingSchema],
     averageRating: { type: Number, default: 0 }
+=======
+
+const bookSchema = mongoose.Schema ({
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    title: {type: String, required: true, unique: true},
+    author: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    year: {type: Number, required: true},
+    genre: {type: String, required: true},
+    ratings: [
+        {
+            userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true},
+            grade: {type: Number, required: true}
+    }],
+    averageRating: { type: Number, default: 0}
+>>>>>>> parent of de92fa7 (corretion adding book)
 });
 
-bookSchema.pre('save', function(next) {
+bookSchema.post('save', function(doc, next) {
     if (this.ratings.length > 0) {
         const sum = this.ratings.reduce((acc, curr) => acc + curr.grade, 0);
         this.averageRating = sum / this.ratings.length;
     } else {
         this.averageRating = 0;
     }
-    next();
+    this.save().then(() => next());
 });
-
-bookSchema.methods.updateAverageRating = async function() {
-    if (this.ratings.length > 0) {
-        const sum = this.ratings.reduce((acc, curr) => acc + curr.grade, 0);
-        this.averageRating = sum / this.ratings.length;
-    } else {
-        this.averageRating = 0;
-    }
-    await this.save();
-};
 
 module.exports = mongoose.model ('Book', bookSchema);
